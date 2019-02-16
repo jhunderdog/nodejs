da53c063c9f0b32475ab8e096df6b9534593280f
David <33382water@gmail.com>
2019-02-16 16:05:33 +0900

    mongoDB connect

--- App.js
+++ App.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+const db = require('./config/key').mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('mongoDB Connect'))
+    .catch(err => console.log(err));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 // app.use((req,res,next) => {
 //     res.status(200).json({
@@ -7,10 +26,19 @@ const app = express();
 //     });
 // });
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+app.use((req,res,next) => {
+    const error = new Error("Not found!");
+    error.status = 404;
+    next(error);
+});
 
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
+app.use((error,req,res,next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
--- api/routes/Products.js
+++ api/routes/Products.js
@@ -1,5 +1,7 @@
 const express= require('express');
 const router= express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 router.get( '/', (req,res,next)=>{
     res.status(200).json({
@@ -7,10 +9,32 @@ router.get( '/', (req,res,next)=>{
     });
 });
 
-router.post( '/', (req,res,next)=>{
-    res.status(201).json({
-        message:"handling post requests to /products"
+router.post( '/', (req,res,next)=> {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
     });
+
+    product
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message:"Created product successfully",
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        });
+
+    // res.status(201).json({
+    //     message:"handling post requests to /products",
+    //     createdProduct: product
+    // });
 });
 
 router.post( '/:productId', (req,res,next)=>{
